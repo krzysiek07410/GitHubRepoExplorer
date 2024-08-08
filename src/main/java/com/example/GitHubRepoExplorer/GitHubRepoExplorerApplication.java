57c851cebecb3d44e33d0eb7769dfb1dcb98ccd6
Krzysztof Szymczyk <s23210@pjwstk.edu.pl>
2024-08-08 14:42:01 +0200

    Initial commit

---
+++ src/main/java/com/example/GitHubRepoExplorer/GitHubRepoExplorerApplication.java
@@ -0,0 +1,13 @@
+package com.example.GitHubRepoExplorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubRepoExplorerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitHubRepoExplorerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/GitHubRepoExplorer/GitHubRepoExplorerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GitHubRepoExplorer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubRepoExplorerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
