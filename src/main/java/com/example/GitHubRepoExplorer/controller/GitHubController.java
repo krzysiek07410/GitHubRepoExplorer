d666f6fbe7310c91bc191847a0b95afcb661f67d
Krzysztof Szymczyk <s23210@pjwstk.edu.pl>
2024-08-09 13:20:34 +0200

    add @RequestHeader and check  for accept header

--- src/main/java/com/example/GitHubRepoExplorer/controller/GitHubController.java
+++ src/main/java/com/example/GitHubRepoExplorer/controller/GitHubController.java
@@ -2,17 +2,16 @@ package com.example.GitHubRepoExplorer.controller;
 
 import com.example.GitHubRepoExplorer.domain.Repository;
 import com.example.GitHubRepoExplorer.service.GitHubService;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class GitHubController {
+    private static final String MEDIA_TYPE = "application/json";
     private final GitHubService gitHubService;
 
     public GitHubController(GitHubService gitHubService) {
@@ -20,7 +19,10 @@ public class GitHubController {
     }
 
     @GetMapping("/repos/{username}")
-    public ResponseEntity<List<Repository>> getRepositories(@PathVariable String username) {
+    public ResponseEntity<List<Repository>> getRepositories(@PathVariable String username, @RequestHeader(HttpHeaders.ACCEPT) String acceptHeader) {
+        if (!MEDIA_TYPE.equals(acceptHeader)) {
+            return ResponseEntity.badRequest().build();
+        }
         List<Repository> repositories = gitHubService.getNonForkedRepositoriesByUsername(username);
         return ResponseEntity.ok(repositories);
     }
