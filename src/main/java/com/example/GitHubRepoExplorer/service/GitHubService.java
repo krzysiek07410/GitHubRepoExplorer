097f44e99aa78f1923ab40044c501d46f309bfff
Krzysztof Szymczyk <s23210@pjwstk.edu.pl>
2024-08-09 13:01:25 +0200

    extract fetching and parsing logic to separate classes

--- src/main/java/com/example/GitHubRepoExplorer/service/GitHubService.java
+++ src/main/java/com/example/GitHubRepoExplorer/service/GitHubService.java
@@ -6,37 +6,23 @@ import com.example.GitHubRepoExplorer.domain.Owner;
 import com.example.GitHubRepoExplorer.domain.Repository;
 import com.example.GitHubRepoExplorer.dto.BranchDTO;
 import com.example.GitHubRepoExplorer.dto.RepositoryDTO;
-import com.example.GitHubRepoExplorer.exception.UserNotFoundException;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
+import com.example.GitHubRepoExplorer.utils.GitHubApiClient;
+import com.example.GitHubRepoExplorer.utils.JsonUtils;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.reactive.function.client.ClientResponse;
-import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
 
 import java.util.List;
 
 @Service
 public class GitHubService {
     private static final String USER_REPOS_URL_TEMPLATE = "/users/%s/repos";
-    private static final String USER_NOT_FOUND_EXCEPTION_MESSAGE = "User not found";
-    private static final String JSON_PARSE_FAILED_MESSAGE = "Failed to parse json";
-    private static final String headerValue = "application/vnd.github.v3+json";
     private static final String EMPTY_VALUE = "";
 
-    private final WebClient webClient;
-    private final ObjectMapper objectMapper;
-    private final String authToken;
+    private final GitHubApiClient gitHubApiClient;
+    private final JsonUtils jsonUtils;
 
-    public GitHubService(WebClient webClient, ObjectMapper objectMapper, @Value("${github.token}") String authToken) {
-        this.webClient = webClient;
-        this.objectMapper = objectMapper;
-        this.authToken = authToken;
+    public GitHubService(GitHubApiClient gitHubApiClient, JsonUtils jsonUtils) {
+        this.gitHubApiClient = gitHubApiClient;
+        this.jsonUtils = jsonUtils;
     }
 
     public List<Repository> getNonForkedRepositoriesByUsername(String username) {
@@ -63,65 +49,21 @@ public class GitHubService {
     }
 
     private List<RepositoryDTO> fetchRepositories(String url) {
-        try {
-            String responseBody = makeApiRequest(url);
-            return parseJsonToList(responseBody, RepositoryDTO[].class);
-        } catch (HttpClientErrorException e) {
-            handleWebClientError(e);
-            return List.of();
-        }
+        String responseBody = gitHubApiClient.makeApiRequest(url);
+        return jsonUtils.parseJsonToList(responseBody, RepositoryDTO[].class);
     }
 
     private List<Branch> getBranches(String branchesUrl) {
-        String responseBody = makeApiRequest(branchesUrl);
-        List<BranchDTO> branchDTOS = parseJsonToList(responseBody, BranchDTO[].class);
+        String responseBody = gitHubApiClient.makeApiRequest(branchesUrl);
+        List<BranchDTO> branchDTOS = jsonUtils.parseJsonToList(responseBody, BranchDTO[].class);
         return branchDTOS.stream()
                 .map(this::mapToBranch)
                 .toList();
-    }
-
-    private <T> List<T> parseJsonToList(String responseBody, Class<T[]> tClass) {
-        try {
-            return List.of(objectMapper.readValue(responseBody, tClass));
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(JSON_PARSE_FAILED_MESSAGE, e);
-        }
     }
 
     private Branch mapToBranch(BranchDTO branchDTO) {
         String branchName = branchDTO.getName();
         String lastCommitSha = branchDTO.getCommit().getSha();
         return new Branch(branchName, new Commit(lastCommitSha));
-    }
-
-    private String makeApiRequest(String url) {
-        return webClient.get()
-                .uri(url)
-                .headers(this::setHeaders)
-                .retrieve()
-                .onStatus(HttpStatusCode::isError, this::handleErrorResponse)
-                .bodyToMono(String.class)
-                .block();
-    }
-
-    private void setHeaders(HttpHeaders headers) {
-        headers.set(HttpHeaders.ACCEPT, headerValue);
-        if (!authToken.isEmpty()) {
-            headers.set(HttpHeaders.AUTHORIZATION, "Bearer " + authToken);
-        }
-    }
-
-    private Mono<? extends Throwable> handleErrorResponse(ClientResponse clientResponse) {
-        if (clientResponse.statusCode() == HttpStatus.NOT_FOUND) {
-            return Mono.error((new UserNotFoundException(USER_NOT_FOUND_EXCEPTION_MESSAGE)));
-        }
-        return clientResponse.createException();
-    }
-
-    private void handleWebClientError(HttpClientErrorException e) {
-        if (e.getStatusCode() == HttpStatus.NOT_FOUND) {
-            throw new UserNotFoundException(USER_NOT_FOUND_EXCEPTION_MESSAGE);
-        }
-        throw e;
     }
 }
---
+++ src/main/java/com/example/GitHubRepoExplorer/utils/GitHubApiClient.java
@@ -0,0 +1,49 @@
+package com.example.GitHubRepoExplorer.utils;
+
+import com.example.GitHubRepoExplorer.exception.UserNotFoundException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.ClientResponse;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@Component
+public class GitHubApiClient {
+    private static final String USER_NOT_FOUND_EXCEPTION_MESSAGE = "User not found";
+    private static final String headerValue = "application/vnd.github.v3+json";
+
+    private final WebClient webClient;
+    private final String authToken;
+
+    public GitHubApiClient(WebClient webClient, @Value("${github.token}") String authToken) {
+        this.webClient = webClient;
+        this.authToken = authToken;
+    }
+
+    public String makeApiRequest(String url) {
+        return webClient.get()
+                .uri(url)
+                .headers(this::setHeaders)
+                .retrieve()
+                .onStatus(HttpStatusCode::isError, this::handleErrorResponse)
+                .bodyToMono(String.class)
+                .block();
+    }
+
+    private void setHeaders(HttpHeaders headers) {
+        headers.set(HttpHeaders.ACCEPT, headerValue);
+        if (!authToken.isEmpty()) {
+            headers.set(HttpHeaders.AUTHORIZATION, "Bearer " + authToken);
+        }
+    }
+
+    private Mono<? extends Throwable> handleErrorResponse(ClientResponse clientResponse) {
+        if (clientResponse.statusCode() == HttpStatus.NOT_FOUND) {
+            return Mono.error(new UserNotFoundException(USER_NOT_FOUND_EXCEPTION_MESSAGE));
+        }
+        return clientResponse.createException();
+    }
+}
